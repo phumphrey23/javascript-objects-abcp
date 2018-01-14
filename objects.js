00ea9ed2f97cc776afab8e2db08cfe68c932c056
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-14 16:16:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,7 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName){
-   delete playlist.artistName;
+   delete playlist.Slowdive;
    return playlist
 };
-removeFromPlaylist(playlist, 'Slowdive');
