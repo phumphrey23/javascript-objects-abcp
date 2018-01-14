320fe86d602e2f790a5e17d4e664477d02e0d786
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-14 16:11:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,11 +3,12 @@ var playlist = {
   'My Bloody Valentine': 'Sometimes'
 };
 function updatePlaylist(playlist, artistName, songTitle){
-   Object.assign({},playlist,{ [artistName]: songTitle });
-   return playlist
+   return Object.assign({},playlist,{ [artistName]: songTitle });
 };
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
-function removeFromPlaylist(){
-
+function removeFromPlaylist(playlist, artistName){
+   delete playlist.artistName;
+   return playlist
 };
+removeFromPlaylist(playlist, 'Slowdive');
